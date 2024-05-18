103e0ce7cd3bef98118e27207e371f461fa4972d
SyfaAulia394 <167854397+SyfaAulia394@users.noreply.github.com>
2024-05-18 12:32:59 +0700

    Add files via upload

---
+++ HalloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_2301081017;
+
+/**
+ *
+ * @author AULIA SYFA
+ */
+public class HalloWorld {
+     public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Syfa Aulia");
+    }
+}
+
